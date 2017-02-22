79150aae7749329b8757d8aa41d92f7f6c513fb1
noufali <na1215@nyu.edu>
2017-02-22 11:56:52 -0500

    Delete twitter_keys.js

--- twitter_keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	consumer_key:         'XXX',
-    consumer_secret:      'XXX',
-    access_token:         'XXX',
-    access_token_secret:  'XXX',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
-}
